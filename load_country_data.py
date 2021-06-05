bfdfc463c7c9af84b34ed977051b8fe91c085c2c
Jordan <tippet2@pdx.edu>
2021-06-05 15:38:56 +0000

    added timing to the load_country_data file

--- load_country_data.py
+++ load_country_data.py
@@ -1,6 +1,9 @@
 import json
 import redis
+import time
 
+
+start_time = time.perf_conter()
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 with open('../Eurovision_files/Eurovision3.json') as f:
@@ -33,3 +36,6 @@ for tweet in tweets:
 
 for key, value in country_dictionary.items():
     r.zadd("country-test",{key: value}, incr=True)
+
+total_time = time.perf_counter() - start_time
+print(f"The program took {total_time:0.2f} seconds to run")
