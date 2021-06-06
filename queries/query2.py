8347fd32895e7d6cd8797ccbf027d138d91b699b
Jordan <tippet2@pdx.edu>
2021-06-06 21:41:34 +0000

    updated the code to match the correct sorted set

--- queries/query2.py
+++ queries/query2.py
@@ -4,7 +4,8 @@ import time
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 start_time = time.perf_counter()
-countries = r.zrevrange("country-test", 0, 0, withscores=True)
+countries = r.zrevrange("Country", 0, 0, withscores=True)
 print(countries)
 total_time = time.perf_counter() - start_time
 print(f"Query executed in {total_time:0.2f} seconds")
+
